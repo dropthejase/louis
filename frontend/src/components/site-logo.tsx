d416d4ffab1c837c85471917479d34c72ab87722
Jason Lee <12leeejk2@gmail.com>
2026-05-07 23:11:58 +0100

    chore(frontend): rename app from Mike to Louis in all user-facing text

--- frontend/src/components/site-logo.tsx
+++ frontend/src/components/site-logo.tsx
@@ -39,7 +39,7 @@ export function SiteLogo({
             } ${className}`}
         >
             <MikeIcon size={iconSizes[size]} />
-            <span>Mike</span>
+            <span>Louis</span>
         </h1>
     );
 
